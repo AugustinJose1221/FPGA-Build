f53a59045116b99055f59df403b46f6331b3c1ef
AugustinJose1221 <augustinjose1221@gmail.com>
2021-04-28 14:35:20 +0530

    Updated design/filter5x5.v

--- design/filter5x5.v
+++ design/filter5x5.v
@@ -18,8 +18,12 @@ output [15:0]       Dout
 
 
 reg [7:0] storage[0:N*M-1];                                                   //reg array to store the image pixels in a frame of operation
-reg [7:0] image_kernal[0:8];                                                  //reg array to house the 9 bytes for the sobel convolution
+reg [7:0] image_kernal[0:24];                                                  //reg array to house the 9 bytes for the sobel convolution
 reg [15:0] result;
+reg [15:0] result0;
+reg [15:0] result1;
+reg [15:0] result2;
+reg [15:0] result3;
 reg [1:0] PS, NS;
 
 integer count, i, j, k;
@@ -52,6 +56,10 @@ begin
  case (PS)
  IDLE: begin
         result = 16'h0000;
+        result0 = 16'h0000;
+        result1 = 16'h0000;
+        result2 = 16'h0000;
+        result3 = 16'h0000;
         count = 0;  //to count the number of convolutions in a frame
         j = 0;
         k = 0;
@@ -68,21 +76,49 @@ begin
        image_kernal[0] = storage[j];
        image_kernal[1] = storage[j+1];
        image_kernal[2] = storage[j+2];
-       image_kernal[3] = storage[j+5];
-       image_kernal[4] = storage[j+6];
-       image_kernal[5] = storage[j+7];
-       image_kernal[6] = storage[j+10];
-       image_kernal[7] = storage[j+11];
-       image_kernal[8] = storage[j+12];
+       image_kernal[3] = storage[j+3];
+       image_kernal[4] = storage[j+4];
+
+       image_kernal[5] = storage[j+M];
+       image_kernal[6] = storage[j+M+1];
+       image_kernal[7] = storage[j+M+2];
+       image_kernal[8] = storage[j+M+3];
+       image_kernal[9] = storage[j+M+4];
+
+       image_kernal[10] = storage[j+(2*M)];
+       image_kernal[11] = storage[j+(2*M)+1];
+       image_kernal[12] = storage[j+(2*M)+2];
+       image_kernal[13] = storage[j+(2*M)+3];
+       image_kernal[14] = storage[j+(2*M)+4];
+
+       image_kernal[15] = storage[j+(3*M)];
+       image_kernal[16] = storage[j+(3*M)+1];
+       image_kernal[17] = storage[j+(3*M)+2];
+       image_kernal[18] = storage[j+(3*M)+3];
+       image_kernal[19] = storage[j+(3*M)+4];
+
+       image_kernal[20] = storage[j+(4*M)];
+       image_kernal[21] = storage[j+(4*M)+1];
+       image_kernal[22] = storage[j+(4*M)+2];
+       image_kernal[23] = storage[j+(4*M)+3];
+       image_kernal[23] = storage[j+(4*M)+4];
 
        NS = CONVOLUTE;
       end
  CONVOLUTE: begin
-             result = image_kernal[0] + 2*image_kernal[1] + image_kernal[2] - image_kernal[6] - 2*image_kernal[7] - image_kernal[8];
+             // sigma = 3
+             result0 = (0.0113 * image_kernal[0]) + (0.0134 * image_kernal[1]) + (0.0142 * image_kernal[2]) + (0.0134 * image_kernal[3]) + (0.0113 * image_kernal[4]) + (0.0134 * image_kernal[5]) + (0.0158 * image_kernal[6]) + (0.0167 * image_kernal[7]) + (0.0158 * image_kernal[8]) + (0.0134 * image_kernal[9]) + (0.0142 * image_kernal[10]) + (0.0167 * image_kernal[11]) + (0.0177 * image_kernal[12]) + (0.0167 * image_kernal[13]) + (0.0142 * image_kernal[14]) + (0.0134 * image_kernal[15]) + (0.0158 * image_kernal[16]) + (0.0167 * image_kernal[17]) + (0.0158 * image_kernal[18]) + (0.0134 * image_kernal[19]) + (0.0113 * image_kernal[20]) + (0.0134 * image_kernal[21]) + (0.0142 * image_kernal[22]) + (0.0134 * image_kernal[23]) + (0.0113 * image_kernal[24]);
+             // sigma = 5
+             result1 = (0.0054 * image_kernal[0]) + (0.0058 * image_kernal[1]) + (0.0059 * image_kernal[2]) + (0.00058 * image_kernal[3]) + (0.0054 * image_kernal[4]) + (0.0058 * image_kernal[5]) + (0.0061 * image_kernal[6]) + (0.0062 * image_kernal[7]) + (0.0061 * image_kernal[8]) + (0.0058 * image_kernal[9]) + (0.0059 * image_kernal[10]) + (0.0062 * image_kernal[11]) + (0.0064 * image_kernal[12]) + (0.0062 * image_kernal[13]) + (0.0059 * image_kernal[14]) + (0.0058 * image_kernal[15]) + (0.0061 * image_kernal[16]) + (0.0062 * image_kernal[17]) + (0.0061 * image_kernal[18]) + (0.0058 * image_kernal[19]) + (0.0054 * image_kernal[20]) + (0.0058 * image_kernal[21]) + (0.0059 * image_kernal[22]) + (0.0058 * image_kernal[23]) + (0.0054 * image_kernal[24]);
+
+             //result2 = (0.0113 * image_kernal[0]) + (0.0134 * image_kernal[1]) + (0.0142 * image_kernal[2]) + (0.0134 * image_kernal[3]) + (0.0113 * image_kernal[4]) + (0.0134 * image_kernal[5]) + (0.0158 * image_kernal[6]) + (0.0167 * image_kernal[7]) + (0.0158 * image_kernal[8]) + (0.0134 * image_kernal[9]) + (0.0142 * image_kernal[10]) + (0.0167 * image_kernal[11]) + (0.0177 * image_kernal[12]) + (0.0167 * image_kernal[13]) + (0.0142 * image_kernal[14]) + (0.0134 * image_kernal[15]) + (0.0158 * image_kernal[16]) + (0.0167 * image_kernal[17]) + (0.0158 * image_kernal[18]) + (0.0134 * image_kernal[19]) + (0.0113 * image_kernal[20]) + (0.0134 * image_kernal[21]) + (0.0142 * image_kernal[22]) + (0.0134 * image_kernal[23]) + (0.0113 * image_kernal[24]);
+
+             //result3 = (0.0113 * image_kernal[0]) + (0.0134 * image_kernal[1]) + (0.0142 * image_kernal[2]) + (0.0134 * image_kernal[3]) + (0.0113 * image_kernal[4]) + (0.0134 * image_kernal[5]) + (0.0158 * image_kernal[6]) + (0.0167 * image_kernal[7]) + (0.0158 * image_kernal[8]) + (0.0134 * image_kernal[9]) + (0.0142 * image_kernal[10]) + (0.0167 * image_kernal[11]) + (0.0177 * image_kernal[12]) + (0.0167 * image_kernal[13]) + (0.0142 * image_kernal[14]) + (0.0134 * image_kernal[15]) + (0.0158 * image_kernal[16]) + (0.0167 * image_kernal[17]) + (0.0158 * image_kernal[18]) + (0.0134 * image_kernal[19]) + (0.0113 * image_kernal[20]) + (0.0134 * image_kernal[21]) + (0.0142 * image_kernal[22]) + (0.0134 * image_kernal[23]) + (0.0113 * image_kernal[24]);
+             result = result0 - result1;
              count = count + 1;
-             if(count != (N-2)*(M-2))
+             if(count != (N-4)*(M-4))
              begin
-              if(k != (M-2))
+              if(k != (M-4))
               begin
                j = j + 1;
                k = k + 1;
@@ -90,7 +126,7 @@ begin
               end
               else
               begin
-               j = j + 3;
+               j = j + 5;
                k = 0;
                NS = FIX;
               end
